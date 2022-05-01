cdb8255dfd74fd46a2ffef0d771ee0990e792ed5
12Wass <wassimdah@gmail.com>
2022-05-01 14:22:00 +0200

    refactoring banner component : externalizing long tailwind classes and adding react-scroll

---
+++ frontend/src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        "Something"
+    )
+}
+
+export default AboutMe;
--- frontend/src/components/Banner/Banner.tsx
+++ frontend/src/components/Banner/Banner.tsx
@@ -2,27 +2,27 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <section className="text-white bg-gray-900">
-            <div className="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
-                <div className="max-w-3xl mx-auto text-center">
-                    <h1 className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+        <section className="section-banner-container">
+            <div className="div-banner-container">
+                <div className="div-banner-text-container">
+                    <h1 className="section-title">
                         Wassim Dahmane
                         <span className="sm:block">
                           Développeur full-stack
                         </span>
                     </h1>
 
-                    <p className="max-w-xl mx-auto mt-4 sm:leading-relaxed sm:text-xl">
+                    <p className="banner-introduction-text">
                         Je sais pas encore quoi écrire, laissez moi un peu de temps histoire d'improviser un truc propre et attrayant
                     </p>
 
-                    <div className="flex flex-wrap justify-center gap-4 mt-8">
-                        <a className="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded sm:w-auto active:text-opacity-75 hover:bg-transparent hover:text-white focus:outline-none focus:ring"
+                    <div className="div-banner-button-container">
+                        <a className="banner-bouncing-button"
                            href="/discover-me">
                             Découvrez-moi !
                         </a>
 
-                        <a className="block w-full px-12 py-3 text-sm font-medium text-white border border-blue-600 rounded sm:w-auto hover:bg-blue-600 active:bg-blue-500 focus:outline-none focus:ring"
+                        <a className="banner-button"
                            href="/contact">
                             Contact
                         </a>
@@ -34,9 +34,3 @@ const Banner = () => {
 }
 
 export default Banner;
-
-
-/*
-$nodeList = [1, 7, 3, 4, 2, 6, 9];
-$nodeLinks = [3, 3, 4, 6, 6, 9, 5];
- */
