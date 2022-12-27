1333eb3f3c6958b102483d22663095b2665b9ed4
12Wass <wassimdah@gmail.com>
2022-12-27 02:47:10 +0100

    Updating Skills component to match Educational background usage. Still needs improvements and maybe will have to separate both components. Editing styling too to make it dynamic depending on how much child skill there is.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import Banner from "./components/Banner/Banner";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Skills from "./components/Skills/Skills";
+import Separator from './components/Separator/Separator';
 
 function App() {
     return (
@@ -12,22 +13,48 @@ function App() {
             <Banner/>
             <AboutMe/>
             <Skills
+                context={
+                    {
+                        title: "Expérience professionnelle",
+                        description: "A travers mes différentes expériences professionnelles et mes projets personnels et scolaires, j'ai pu m'ouvrir à de nombreuses façons de progammer mais surtout de penser"
+                    }
+                }
                 skills={[
                     {
                         enterprise: "DGAC - SNA-RP",
                         description: "Développement d'une application critique permettant de gérer le cycle de travail des contrôleurs aériens de toute la France et des Outre-mers",
-                        modalDetails: "Modal details"
+                        responsibility: "Fullstack Developer"
                     },
                     {
                         enterprise: "Finelia",
-                        description: "Développement d'une application de Trade Finance permettant aux entreprises de gérer leurs transactions financières via SWIFT",
-                        modalDetails: "Modal details"
+                        description: "Développement d'une application de Trade Finance permettant aux entreprises de gérer leurs transactions financières et leurs garanties via SWIFT",
+                        responsibility: "Fullstack Developer"
                     },
                     {
                         enterprise: "Teksial",
-                        description: "Participation au développement de plusieurs applications internes de gestion de client et de front-office",
-                        modalDetails: "Modal details"
+                        description: "Participation au développement de plusieurs applications internes de gestion de client dans le contexte des Certificats d'Économie d'Énergie",
+                        responsibility: "Fullstack Developer"
                     }
+                ]}
+            />
+            <Skills
+                context={
+                    {
+                        title: "Parcours scolaire",
+                        description: "Alumni à l'ESGI, j'ai eu l'occasion de réaliser beaucoup de projets très intéressants dans le cadre de mes études mais également pendant mon temps libre"
+                    }
+                }
+                skills={[
+                    {
+                        enterprise: "BonApp",
+                        description: "Création d'une application de gestion de commande et de paiement dans un restaurant via QR Code avec un système complet de gestion d'équipes",
+                        responsibility: "CEO - Developer"
+                    },
+                    {
+                        enterprise: "Rocket Launcher",
+                        description: "Développement d'une application de mise en relation entre influenceurs et entreprises",
+                        responsibility: "Project Manager - Developer"
+                    },
                 ]}
             />
         </div>
--- frontend/src/components/Skills/Skills.tsx
+++ frontend/src/components/Skills/Skills.tsx
@@ -1,64 +1,57 @@
 import React, {ReactElement} from 'react';
-// import Modal from "../Modal/Modal";
-
+import {Link} from 'react-scroll';
 
 interface Props {
     skills: {
         enterprise: string,
         description: string,
-        modalDetails: string, // CHANGE THIS TO JSON OBJECT (IMAGES AND ALL OF THAT)
+        responsibility: string, // CHANGE THIS TO JSON OBJECT (IMAGES AND ALL OF THAT)
     }[];
+    context?: {
+        title: string,
+        description: string,
+    };
 }
 
 const Skills: React.FC<Props> = (skills: Props): ReactElement => {
-
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-    // const [modalData, setModalData] = useState();
-
-    // const toggleModal = (data: any) => {
-    //     setModalData(data);
-    //     if (isModalOpen === false) {
-    //         setIsModalOpen(!isModalOpen);
-    //     }
-    // }
-
-
     return (
         <section className="text-white bg-gray-900 relative">
             <div className="skills-div-container">
                 <div className="max-w-lg mx-auto text-center">
-                    <h2 className="text-3xl font-bold sm:text-4xl">Expérience professionnelle</h2>
+                    <h2 className="text-3xl font-bold sm:text-4xl">{skills.context?.title}</h2>
 
                     <p className="mt-4 text-gray-300">
-                        A travers mes différentes expériences professionnelles et mes projets personnels et scolaires,
-                        j'ai pu m'ouvrir à de nombreuses façons de programmer mais surtout de penser.
+                        {skills.context?.description}
                     </p>
                 </div>
-                <div className="skills-card-grid">
+                <div className={`skills-card-grid lg:grid-cols-${skills.skills.length}`}>
                     {skills.skills.map(skill => {
                         return (
                             <button
                                 className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-blue-500/10 hover:border-blue-500/10"
                                 key={skill.enterprise}
                             >
-                            <div className="w-10 h-10 text-blue-500">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path d="M12 14l9-5-9-5-9 5 9 5z"/>
-                                    <path
-                                        d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                                    />
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                                    />
-                                </svg>
+                            <div>
+                                <span className='float-right pt-2 font-extralight'>{skill.responsibility}</span>
+                                <div className="w-10 h-10 text-blue-500">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path d="M12 14l9-5-9-5-9 5 9 5z"/>
+                                        <path
+                                            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+                                        />
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                                        />
+                                    </svg>
+                                </div>
                             </div>
 
                                 <h3 className="mt-4 text-xl font-bold text-white">{skill.enterprise}</h3>
