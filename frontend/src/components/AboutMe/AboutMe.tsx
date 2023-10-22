33a7cea0d5f2998beb3b05c7b24e78c89863d158
12Wass <wassimdah@gmail.com>
2023-10-21 17:54:32 -0700

    Yarn upgrade

--- frontend/src/components/AboutMe/AboutMe.tsx
+++ frontend/src/components/AboutMe/AboutMe.tsx
@@ -1,55 +1,53 @@
-import React, {ReactElement} from 'react';
-import {Element} from "react-scroll";
+import React, { ReactElement } from "react";
+import { Element } from "react-scroll";
 import Separator from "../Separator/Separator";
 import "./AboutMe.css";
 
 const AboutMe: React.FC = (): ReactElement => {
-    return (
-        <Element name="who-am-i">
-            <div className="max-w-5xl px-4 mx-auto lg:h-max">
-                <section className="p-8 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg">
-                    <div className="grid grid-cols-1 gap-12 sm:grid-cols-3 sm:items-center">
-                        <div className="relative">
-                            <div className="aspect-w-1 aspect-h-1">
-                                <img
-                                    src="https://media.licdn.com/dms/image/C4D03AQHQQ7ZKm4cSNw/profile-displayphoto-shrink_800_800/0/1602625687632?e=1676505600&v=beta&t=GX9f0V-iYxM5gTy5hWnTZzYR55pDtPCqZGtQkCEqq28"
-                                    alt=""
-                                    className="object-cover rounded-lg"
-                                />
-                            </div>
+  return (
+    <Element name="who-am-i">
+      <div className="max-w-5xl px-4 mx-auto lg:h-max">
+        <section className="p-8 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg">
+          <div className="grid grid-cols-1 gap-12 sm:grid-cols-3 sm:items-center">
+            <div className="relative">
+              <div className="aspect-w-1 aspect-h-1">
+                <img
+                  src="https://media.licdn.com/dms/image/D4E03AQE0Z2YJIOPATg/profile-displayphoto-shrink_400_400/0/1674605710603?e=1703116800&v=beta&t=wHxiVk1Ea9v-G9SJUUDOIto0O8IJ-b-pSOeNtp5Nny8"
+                  alt=""
+                  className="object-cover rounded-lg"
+                />
+              </div>
 
-                            <div
-                                className="absolute inline-flex px-4 py-2 bg-gradient-to-r from-blue-500 to-cyan-500 rounded-lg shadow-xl -bottom-4 -right-4"
-                            >
-                                <span className="inline-block bg-transparent rounded-lg">
-                                    <a className="ml-2 mr-2" href="#here">
-                                        Télécharger mon CV
-                                    </a>
-                                </span>
-                            </div>
-                        </div>
-
-                        <blockquote className="sm:col-span-2">
-                            <p className="text-xl font-medium sm:text-2xl">
-                                Passionné et très curieux. J'essaie d'atteindre les limites de stockage de mon cerveau
-                                pour
-                                voir.
-                            </p>
-
-                            <cite className="inline-flex items-center mt-8 not-italic">
-                                <span className="hidden w-6 h-px bg-gray-400 sm:inline-block"/>
-                                <p className="text-sm text-gray-500 uppercase sm:ml-3">
-                                    <strong>Wassim Dahmane</strong>, &#123;&#123; Votre entreprise &#125;&#125;.
-                                </p>
-                            </cite>
-                        </blockquote>
-                    </div>
-                </section>
-
-                <Separator/>
+              <div className="absolute inline-flex px-4 py-2 bg-gradient-to-r from-blue-500 to-cyan-500 rounded-lg shadow-xl -bottom-4 -right-4">
+                <span className="inline-block bg-transparent rounded-lg">
+                  <a className="ml-2 mr-2" href="#here">
+                    Télécharger mon CV
+                  </a>
+                </span>
+              </div>
             </div>
-        </Element>
-    )
-}
+
+            <blockquote className="sm:col-span-2">
+              <p className="text-xl font-medium sm:text-2xl">
+                Passionné et très curieux. J'essaie d'atteindre les limites de
+                stockage de mon cerveau pour voir.
+              </p>
+
+              <cite className="inline-flex items-center mt-8 not-italic">
+                <span className="hidden w-6 h-px bg-gray-400 sm:inline-block" />
+                <p className="text-sm text-gray-500 uppercase sm:ml-3">
+                  <strong>Wassim Dahmane</strong>, &#123;&#123; Votre entreprise
+                  &#125;&#125;.
+                </p>
+              </cite>
+            </blockquote>
+          </div>
+        </section>
+
+        <Separator />
+      </div>
+    </Element>
+  );
+};
 
 export default AboutMe;
